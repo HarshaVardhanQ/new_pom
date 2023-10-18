930e99bf0816fbeb33a2db0d11829121e0b055d4
Veera Harshavardhan <134594644+HarshaVardhanQ@users.noreply.github.com>
2023-10-18 10:33:30 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,28 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class BankingApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter principal amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.print("Enter annual interest rate (as a decimal): ");
+        double annualInterestRate = scanner.nextDouble();
+
+        System.out.print("Enter the number of years: ");
+        int years = scanner.nextInt();
+
+        double compoundInterest = calculateCompoundInterest(principal, annualInterestRate, years);
+
+        System.out.printf("The compound interest after %d years is: $%.2f\n", years, compoundInterest);
+    }
+
+    public static double calculateCompoundInterest(double principal, double annualInterestRate, int years) {
+        double compoundInterest = principal * Math.pow(1 + annualInterestRate, years) - principal;
+        return compoundInterest;
     }
 }
